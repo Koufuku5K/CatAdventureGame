5ac68631270634493bb4639a3c38902c7c464f65
l2t <38781078+NathanaelIanGunadi@users.noreply.github.com>
2022-09-29 21:07:25 +0900

    Update MainMenu.cs

--- Cat Adventure/Cat Adventure/Assets/MainMenu.cs
+++ Cat Adventure/Cat Adventure/Assets/MainMenu.cs
@@ -6,9 +6,14 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Start is called before the first frame update
-    public void PlayGame()
+    public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
     }
 
 }
